5600eecec6db40c9a6edaf3c481670c1bbeff08b
srikanthprabala <srikanthprabala94@gmail.com>
2017-12-15 16:26:24 -0500

    Update Main.py
    Added command line strategy in Main

--- Main.py
+++ Main.py
@@ -2,11 +2,12 @@ import sys
 from Controller import Controller
 from classy_model import Model
 from View import View
+import argparse
 
 class Main(object):
-    def __init__(self):
+    def __init__(self, strategy):
         self.TEST_FLAG = False
-        self.strategy = 1
+        self.strategy = strategy
         self.run()
 
     def run(self):
@@ -18,4 +19,8 @@ class Main(object):
         cobj.set_model(mobj)
 
 if __name__ == '__main__':
-    mobj = Main()
+    parser = argparse.ArgumentParser(description='Set a strategy [1,2,3] beware 1 is fast but only minimizes, default is 1 though')
+    parser.add_argument('--strategy', help='an interger for stategy', default='1')
+    args = parser.parse_args()
+    stratint = args.strategy
+    mobj = Main(int(stratint))
